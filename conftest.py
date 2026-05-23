5896d77845eb532a5a26b56bdecf68cccec89e3c
Milena Grabovskiy <milenagrabovskiy@Milenas-MacBook-Air.local>
2026-05-23 15:27:30 -0400

    modified conftest to work with cicd

--- conftest.py
+++ conftest.py
@@ -1,10 +1,30 @@
+import os
+
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 
 
 @pytest.fixture(scope="class")
 def init_driver(request):
-    driver = webdriver.Chrome()
+    browser = os.getenv("BROWSER", "chrome")
+    headless = os.getenv("HEADLESS", "false").lower() == "true"
+
+    if browser == "chrome":
+        options = ChromeOptions()
+
+        if headless:
+            options.add_argument("--headless=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+
+        driver = webdriver.Chrome(options=options)
+
+    else:
+        raise Exception(f"Unsupported browser: {browser}")
+
+    driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.quit()
